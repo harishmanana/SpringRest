453f3a2a72f2f37699cf7e62ce1f0db04e8c6b25
Harish Manana <harish.manana@stackroute.in>
2020-08-05 00:03:36 +0530

    First Commit

---
+++ src/main/java/com/spring/dao/AbstractDao.java
@@ -0,0 +1,24 @@
+package com.spring.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AbstractDao {
+	 @Autowired
+	    private SessionFactory sessionFactory;
+	 
+	    protected Session getSession() {
+	        return sessionFactory.getCurrentSession();
+	    }
+	 
+	    public void persist(Object entity) {
+//	        getSession().persist(entity);
+	        getSession().save(entity);
+	    }
+	 
+	    public void delete(Object entity) {
+	        getSession().delete(entity);
+	    }
+	    
+}
